f42a358a771ce65dd72f9fa111aac9d575ae1e76
josesanchezmx <montieljose711@gmail.com>
2025-01-13 19:21:13 -0600

    headers

--- main.js
+++ main.js
@@ -1,70 +1,120 @@
-/*console.log('Hello, World')
 
-const URL = "https://api.thecatapi.com/v1/images/search";
+const API_URL_RANDOM = 'https://api.thecatapi.com/v1/images/search?limit=4';
+const API_URL_FAVORITES = 'https://api.thecatapi.com/v1/favourites';
+const API_URL_FAVORITE_DELETE = (id) => `https://api.thecatapi.com/v1/favourites/${id}`;
 
-fetch(URL)
-.then(res => res.json())
-.then(data => {
-    const img = document.querySelector('img');
-    img.src = data[0].url;
-});*/
-
-// limite de imagenes que queremos de la api
-//const API_URL = "https://dog.ceo/api/breeds/image/random/3";
-//const API_URL = "https://dog.ceo/api/breed/malinois/images/random/3";
-const API_URL_RANDOM = 'https://api.thecatapi.com/v1/images/search?limit=2&api_key=live_gDwF2SmsCBk8izRETtwMpYd3mRPuOE6jT0RgAqU3uLMeDaRuBBQ77Yf9pUG4UKE1'
-const API_URL_FAVORITES = 'https://api.thecatapi.com/v1/favourites?limit=2&api_key=live_gDwF2SmsCBk8izRETtwMpYd3mRPuOE6jT0RgAqU3uLMeDaRuBBQ77Yf9pUG4UKE1'
-
-const spanError = document.getElementById('error')
+const spanError = document.getElementById('error');
 
 async function loadRandomMichis() {
-    try {
-        const response = await fetch(API_URL_RANDOM);
-        const data = await response.json();
+  const res = await fetch(API_URL_RANDOM);
+  const data = await res.json();
+  console.log('Random');
+  console.log(data);
 
-        console.log('random michis:');
-        console.log(data)
-        // Acceder a las URLs del array en data.message
-        const img1 = document.getElementById('img1');
-        const img2 = document.getElementById('img2');
-
-        img1.src = data[0].url;// en la de perros es .message
-        img2.src = data[1].url;
-    } catch (error) {
-        console.error('Error fetching cat images:', error);
-    }
-}
-
-async function loadFavouritesMichis() {
-    const res = await fetch(API_URL_FAVORITES);
-    const data = await res.json();
-    console.log('favorites michis:');
-    console.log(data)
+  if (res.status !== 200) {
+    spanError.innerHTML = "Hubo un error: " + res.status;
+  } else {
+    const img1 = document.getElementById('img1');
+    const img2 = document.getElementById('img2');
+    const img3 = document.getElementById('img3');
+    const img4 = document.getElementById('img4');
+    const btn1 = document.getElementById('btn1');
+    const btn2 = document.getElementById('btn2');
+    const btn3 = document.getElementById('btn3');
+    const btn4 = document.getElementById('btn4');
     
-    if ( res.status !== 200) {
-        spanError.innerHTML = 'Error al cargar las imagenes favoritas'+ res.status + data.message;
-    }
+    img1.src = data[0].url;
+    img2.src = data[1].url;
+    img3.src = data[2].url;
+    img4.src = data[3].url;
+
+    btn1.onclick = () => saveFavouriteMichi(data[0].id);
+    btn2.onclick = () => saveFavouriteMichi(data[1].id);
+    btn3.onclick = () => saveFavouriteMichi(data[2].id);
+    btn4.onclick = () => saveFavouriteMichi(data[3].id);
+  }
 }
 
-async function saveFavouriteMichis() {
-    const res = await fetch(API_URL_FAVORITES, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-            image_id: 'h19-vtIeX',
-        }),
-    });
-    const data = await res.json();
+async function loadFavouriteMichis() {
+  const res = await fetch(API_URL_FAVORITES, {
+    method: 'GET',
+    headers: {
+      'X-API-KEY': 'live_gDwF2SmsCBk8izRETtwMpYd3mRPuOE6jT0RgAqU3uLMeDaRuBBQ77Yf9pUG4UKE1',
+    },
+  });
 
-    console.log('Save favourite michis:');
-    console.log(res);
-    if ( res.status !== 200) {
-        spanError.innerHTML = 'Error al cargar las imagenes favoritas'+ res.status + data.message;
+  const data = await res.json();
+  console.log('Favoritos');
+  console.log(data);
+
+  if (res.status !== 200) {
+    spanError.innerHTML = "Hubo un error: " + res.status + data.message;
+  } else {
+    const section = document.getElementById('favoriteMichis');
+    section.innerHTML = ''; // Limpiar los favoritos previos antes de renderizar nuevos
+    const h2 = document.createElement('h2');
+    const h2Text = document.createTextNode('Favoritos');
+    h2.appendChild(h2Text);
+    section.appendChild(h2);
+
+    data.forEach(michi => {
+      const article = document.createElement('article');
+      const img = document.createElement('img');
+      const btn = document.createElement('button');
+      const btnText = document.createTextNode('Sacar al michi de favoritos');
+
+      img.src = michi.image.url;
+      img.width = 150;
+      btn.appendChild(btnText);
+      btn.onclick = () => deleteFavouriteMichi(michi.id);
+      article.appendChild(img);
+      article.appendChild(btn);
+      section.appendChild(article);
+    });
+  }
+}
+
+async function saveFavouriteMichi(id) {
+  const res = await fetch(API_URL_FAVORITES, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'X-API-KEY': 'live_gDwF2SmsCBk8izRETtwMpYd3mRPuOE6jT0RgAqU3uLMeDaRuBBQ77Yf9pUG4UKE1'
+    },
+    body: JSON.stringify({
+      image_id: id
+    }),
+  });
+  const data = await res.json();
+
+  console.log('Save');
+  console.log(res);
+
+  if (res.status !== 200) {
+    spanError.innerHTML = "Hubo un error: " + res.status + data.message;
+  } else {
+    console.log("Michi guardado en favoritos");
+    loadFavouriteMichis(); // Recargar favoritos después de guardar uno nuevo
+  }
+}
+
+// Eliminar favoritos
+async function deleteFavouriteMichi(id) {
+  const res = await fetch(API_URL_FAVORITE_DELETE(id), {
+    method: 'DELETE',
+    headers: {
+        'X-API-KEY': 'live_gDwF2SmsCBk8izRETtwMpYd3mRPuOE6jT0RgAqU3uLMeDaRuBBQ77Yf9pUG4UKE1'
     }
+  });
+  const data = await res.json();
+
+  if (res.status !== 200) {
+    spanError.innerHTML = "Hubo un error: " + res.status + data.message;
+  } else {
+    console.log("Michi eliminado de favoritos");
+    loadFavouriteMichis(); // Recargar favoritos después de eliminar uno
+  }
 }
 
 loadRandomMichis();
-loadFavouritesMichis();
-saveFavouriteMichis();
+loadFavouriteMichis();
